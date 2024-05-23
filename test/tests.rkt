423b6021d6f4030db52fdda24edd04ec7804a615
Ryan Kramer <default.kramer@gmail.com>
2024-05-23 14:29:34 -0500

    Apparently the CMNDAT that accompanies the STGDATs is needed in some situations.

--- test/tests.rkt
+++ test/tests.rkt
@@ -9,7 +9,7 @@
     (load-stage kind (string->path path)))
 
   ; 2024-05-23 looks correct to my eyes
-  (let ([stage (load 'IoA "STGDAT01.001.bin")])
+  (let ([stage (load 'IoA "saves/01/STGDAT01.bin")])
     (check-equal? (blocks-hash stage) '(8305176 9691690))
     (clear-area! stage 'all #:keep-items? #f)
     (repair-sea! stage 'all)
