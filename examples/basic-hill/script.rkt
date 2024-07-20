143370014160a3157a4f74d3210151979481f9cc
Ryan Kramer <default.kramer@gmail.com>
2024-07-20 16:12:35 -0500

    clean up basic-hill example

--- examples/basic-hill/script.rkt
+++ examples/basic-hill/script.rkt
@@ -16,15 +16,17 @@
 
 ; Step 3 - Use bitmap->hill and put-hill!
 (save-dir "C:/Users/kramer/Documents/My Games/DRAGON QUEST BUILDERS II/Steam/76561198073553084/SD/")
+(define source-slot 'B02)
+(define dest-slot 'B00)
 
-(copy-all-save-files! #:from 'B02 #:to 'B00)
+(copy-all-save-files! #:from source-slot #:to dest-slot)
 
-(define B00 (load-stage 'IoA 'B00))
+(define stage (load-stage 'IoA dest-slot))
 
 (define hill (bitmap->hill "hill.bmp"))
-(put-hill! B00 hill (block 'Chert))
+(put-hill! stage hill (block 'Chert))
 
-(save-stage! B00)
+(save-stage! stage)
 
 ; Step 4 - Load it in DQB2 and look around!
 ; If it looks good, maybe you want to copy the result
