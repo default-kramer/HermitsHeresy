9d73c9b97267c769c96ee473640d702c9ba33d90
Ryan Kramer <default.kramer@gmail.com>
2024-07-25 13:06:37 -0500

    Don't repeatedly warn about ambiguous blocks

--- hermits-heresy/hermits-heresy/NEW-API.rkt
+++ hermits-heresy/hermits-heresy/NEW-API.rkt
@@ -67,15 +67,20 @@
     (void)))
 
 (: block (-> Symbol Fixnum))
-(define (block sym)
-  (define blockdef (symbol->blockdef sym))
-  (when (not blockdef)
-    (error "Unknown block:" sym))
-  (define id (blockdef-id blockdef))
-  (define ambiguities (symbol->ambiguities sym))
-  (when ambiguities
-    (show-msg "Choosing (block '~a) to mean ~a, but other values were possible: ~a" sym id ambiguities))
-  id)
+(define block
+  (let ()
+    (define already-warned (ann (make-hash) (Mutable-HashTable Symbol #t)))
+    (lambda ([sym : Symbol])
+      (define blockdef (symbol->blockdef sym))
+      (when (not blockdef)
+        (error "Unknown block:" sym))
+      (define id (blockdef-id blockdef))
+      (define ambiguities (symbol->ambiguities sym))
+      (when (and ambiguities
+                 (not (hash-ref already-warned sym (lambda () #f))))
+        (hash-set! already-warned sym #t)
+        (show-msg "Choosing (block '~a) to mean ~a, but other values were possible: ~a" sym id ambiguities))
+      id)))
 
 #;(define-syntax-rule (define-blocks f [id val IGNORE ...] ...)
     (define (f a)
