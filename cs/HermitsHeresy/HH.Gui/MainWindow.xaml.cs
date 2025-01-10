74e9c8580f3cb24152e9fe2bc408402f04a006a5
Ryan Kramer <default.kramer@gmail.com>
2025-01-10 17:30:38 -0600

    Adding C# code including: Ugly code that produces pretty nice hills. Basic idea for a visual programming experience in WPF.

---
+++ cs/HermitsHeresy/HH.Gui/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System.IO;
+using System.Windows;
+
+namespace HH.Gui
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private void setup()
+		{
+			var rootNode = new BeginVM();
+
+			var cond = rootNode.AddCond();
+			var item1 = cond.AddCondItem();
+			var item2 = cond.AddCondItem();
+			item1.TestExpr = new ScriptNodeWrapper(new TrueVM());
+
+			this.rootse.DataContext = new ScriptNodeWrapper(rootNode);
+			this.rootse.SelectedNodeChanged += Rootse_SelectedNodeChanged;
+		}
+
+		private void Rootse_SelectedNodeChanged(object? sender, ScriptEditorTreeNode.SelectedNodeChangedEventArgs e)
+		{
+			this.nodeDetailEditor.DataContext = e.SelectedItem;
+		}
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			setup();
+		}
+
+		// Actually... it doesn't make sense to open a single STGDAT file! Right?
+		// It's more like you want a "project parameters" tab where you can specify more than
+		// one STGDAT file (e.g. when you want to copy stuff from one stage to another).
+		// Really, what we need is to Open or Save a *.hhproj (Hermit's Heresy project file).
+		private void OnOpenClicked(object sender, RoutedEventArgs e)
+		{
+			var ofd = new Microsoft.Win32.OpenFileDialog();
+			ofd.InitialDirectory = TryFindSD()?.FullName;
+			ofd.Filter = "DQB2 STGDAT files|STGDAT*.BIN|All Files|*.*";
+			var result = ofd.ShowDialog();
+			if (result.GetValueOrDefault())
+			{
+
+			}
+		}
+
+		private static DirectoryInfo? TryFindSD()
+		{
+			var homepath = Environment.GetEnvironmentVariable("HOMEPATH");
+			var username = Environment.GetEnvironmentVariable("USERNAME");
+
+			string path;
+			if (homepath != null)
+			{
+				path = homepath;
+			}
+			else if (username != null)
+			{
+				path = Path.Combine("Users", username);
+			}
+			else
+			{
+				return null;
+			}
+
+			path = Path.Combine(path, "Documents\\My Games\\DRAGON QUEST BUILDERS II\\Steam\\76561198073553084\\SD");
+
+			var homedrive = Environment.GetEnvironmentVariable("HOMEDRIVE") ?? "C:";
+			var dir = new DirectoryInfo(homedrive + path);
+			if (dir.Exists)
+			{
+				return dir;
+			}
+			return null;
+		}
+	}
+}
---
+++ cs/HermitsHeresy/HH.Gui/ScriptNodeViewmodel.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HH.Gui;
+
+/// <summary>
+/// This class handles shared functionality including relocating a node within its parent's
+/// list of children and replacing a node with a different (but compatible) node.
+/// </summary>
+public class ScriptNodeWrapper : INPC
+{
+	private readonly Dictionary<NodeKindVM, ScriptNodeVM> memory = new();
+
+	public ScriptNodeWrapper(ScriptNodeVM content)
+	{
+		SelectedContent = content;
+		selectedContent = SelectedContent; // to avoid the warning
+		CommandMoveDown = new RelayCommand(_ => Parent?.CanMoveChildDown(this) ?? false, _ => Parent?.MoveChildDown(this));
+		CommandMoveUp = new RelayCommand(_ => Parent?.CanMoveChildUp(this) ?? false, _ => Parent?.MoveChildUp(this));
+	}
+
+	private ScriptNodeVM selectedContent;
+	public ScriptNodeVM SelectedContent
+	{
+		get { return selectedContent; }
+		set
+		{
+			selectedContent = value;
+			memory[selectedContent.Kind] = selectedContent;
+			RaisePropertyChanged();
+		}
+	}
+
+	private bool isSelected;
+	public bool IsSelected
+	{
+		get { return isSelected; }
+		set { isSelected = value; RaisePropertyChanged(); }
+	}
+
+	public IEnumerable<NodeKindVM> KindChoices => selectedContent.KindChoices;
+
+	public NodeKindVM SelectedKind
+	{
+		get { return selectedContent.Kind; }
+		set
+		{
+			if (SelectedKind != value)
+			{
+				if (memory.TryGetValue(value, out var vm))
+				{
+					SelectedContent = vm;
+				}
+				else
+				{
+					SelectedContent = value.MakeNode();
+				}
+				RaisePropertyChanged();
+			}
+		}
+	}
+
+	public interface IParentFinder
+	{
+		ScriptNodeWrapper? FindParent();
+	}
+
+	private IParentFinder? parentFinder;
+
+	/// <summary>
+	/// This should be set by the UI when its DataContext is set to this viewmodel.
+	/// </summary>
+	/// <remarks>
+	/// My first instinct was to maintain two-way parent-child references within these
+	/// viewmodels, but that approach actually creates more opportunities for bugs/anomalies.
+	/// Instead, the viewmodels will hold one-way references (parent holds children)
+	/// and this <see cref="IParentFinder"/> mechanism will be used when children need
+	/// to find their parents.
+	/// </remarks>
+	public void SetParentFinder(IParentFinder parentFinder)
+	{
+		this.parentFinder = parentFinder;
+		RaisePropertyChanged(""); // notify all properties
+	}
+
+	public ScriptNodeWrapper? Parent
+	{
+		get
+		{
+			var finder = parentFinder ?? throw new InvalidOperationException("ParentFinder is not set");
+			return finder.FindParent();
+		}
+	}
+
+	public ICommand CommandMoveDown { get; }
+	public ICommand CommandMoveUp { get; }
+
+	protected virtual IList<ScriptNodeWrapper> ChildList => SelectedContent.Children;
+
+	protected virtual bool CanMoveChildDown(ScriptNodeWrapper child)
+	{
+		return CanMoveHelper(ChildList, child, 1, out _, out _);
+	}
+
+	protected virtual bool CanMoveChildUp(ScriptNodeWrapper child)
+	{
+		return CanMoveHelper(ChildList, child, -1, out _, out _);
+	}
+
+	protected virtual void MoveChildDown(ScriptNodeWrapper child)
+	{
+		MoveHelper(ChildList, child, 1);
+	}
+
+	protected virtual void MoveChildUp(ScriptNodeWrapper child)
+	{
+		MoveHelper(ChildList, child, -1);
+	}
+
+	private static bool CanMoveHelper(IList<ScriptNodeWrapper> children, ScriptNodeWrapper child, int delta, out int srcIndex, out int dstIndex)
+	{
+		srcIndex = -1;
+		dstIndex = -1;
+		srcIndex = children.IndexOf(child);
+		if (srcIndex < 0)
+		{
+			return false;
+		}
+		dstIndex = srcIndex + delta;
+		return dstIndex >= 0 && dstIndex < children.Count;
+	}
+
+	private static void MoveHelper(IList<ScriptNodeWrapper> children, ScriptNodeWrapper child, int delta)
+	{
+		if (CanMoveHelper(children, child, delta, out int src, out int dst))
+		{
+			children.RemoveAt(src);
+			children.Insert(dst, child);
+		}
+	}
+}
+
+/// <summary>
+/// Base class for all Script Node viewmodels.
+/// </summary>
+public abstract class ScriptNodeVM : INPC
+{
+	protected ObservableCollection<ScriptNodeWrapper> children = new();
+
+	public IList<ScriptNodeWrapper> Children => children;
+
+	public abstract NodeKindVM Kind { get; }
+
+	public string ShortName => Kind.ShortName;
+
+	/// <summary>
+	/// The complete list of kinds that could replace this node.
+	/// </summary>
+	public IEnumerable<NodeKindVM> KindChoices => PossibleKinds();
+
+	/// <summary>
+	/// See <see cref="KindChoices"/>
+	/// </summary>
+	protected abstract IEnumerable<NodeKindVM> PossibleKinds();
+}
+
+public abstract class StatementVM : ScriptNodeVM
+{
+	protected override IEnumerable<NodeKindVM> PossibleKinds()
+	{
+		yield return NodeKindVM.SetBlock;
+		yield return NodeKindVM.Cond;
+		yield return NodeKindVM.Begin;
+		yield return NodeKindVM.DoNothing;
+	}
+}
+
+public abstract class BooleanVM : ScriptNodeVM
+{
+	protected override IEnumerable<NodeKindVM> PossibleKinds()
+	{
+		yield return NodeKindVM.BlockMatches;
+		yield return NodeKindVM.ChiselMatches;
+		yield return NodeKindVM.InHill;
+		yield return NodeKindVM.InArea;
+		yield return NodeKindVM.False;
+		yield return NodeKindVM.True;
+	}
+}
+
+public class FalseVM : BooleanVM
+{
+	public override NodeKindVM Kind => NodeKindVM.False;
+}
+
+public class TrueVM : BooleanVM
+{
+	public override NodeKindVM Kind => NodeKindVM.True;
+}
+
+public class BeginVM : StatementVM
+{
+	public override NodeKindVM Kind => NodeKindVM.Begin;
+
+	public CondVM AddCond()
+	{
+		var node = new CondVM();
+		var wrapper = new ScriptNodeWrapper(node);
+		children.Add(wrapper);
+		return node;
+	}
+
+	public DoNothingVM AddDoNothing()
+	{
+		var node = new DoNothingVM();
+		var wrapper = new ScriptNodeWrapper(node);
+		children.Add(wrapper);
+		return node;
+	}
+}
+
+public class CondVM : StatementVM
+{
+	public CondItemVM AddCondItem()
+	{
+		var node = new CondItemVM();
+		var wrapper = new ScriptNodeWrapper(node);
+		children.Add(wrapper);
+		return node;
+	}
+
+	public override NodeKindVM Kind => NodeKindVM.Cond;
+}
+
+public class DoNothingVM : StatementVM
+{
+	public override NodeKindVM Kind => NodeKindVM.DoNothing;
+}
+
+public class SetBlockVM : StatementVM
+{
+	private int block = 11;
+	public int Block
+	{
+		get { return block; }
+		set { block = value; RaisePropertyChanged(); }
+	}
+
+	public override NodeKindVM Kind => NodeKindVM.SetBlock;
+}
+
+public class BlockMatchesVM : BooleanVM
+{
+	private int block = 11;
+	public int Block
+	{
+		get { return block; }
+		set { block = value; RaisePropertyChanged(); }
+	}
+
+	public override NodeKindVM Kind => NodeKindVM.BlockMatches;
+}
+
+public class ChiselMatchesVM : BooleanVM
+{
+	public override NodeKindVM Kind => NodeKindVM.ChiselMatches;
+}
+
+public class CondItemVM : ScriptNodeVM
+{
+	private ScriptNodeWrapper _testExpr = new ScriptNodeWrapper(new FalseVM());
+	public ScriptNodeWrapper TestExpr
+	{
+		get { return _testExpr; }
+		set { _testExpr = value; RaisePropertyChanged(); }
+	}
+
+	public ScriptNodeWrapper BodyExpr { get; }
+
+	public CondItemVM()
+	{
+		var begin = new BeginVM();
+		begin.AddDoNothing();
+		BodyExpr = new ScriptNodeWrapper(begin);
+	}
+
+	public override NodeKindVM Kind => NodeKindVM.CondItem;
+
+	protected override IEnumerable<NodeKindVM> PossibleKinds()
+	{
+		yield return NodeKindVM.CondItem;
+	}
+}
+
+public class InHillVM : BooleanVM
+{
+	private string filepath = "";
+	public string Filepath
+	{
+		get { return filepath; }
+		set { filepath = value; RaisePropertyChanged(); }
+	}
+
+	public override NodeKindVM Kind => NodeKindVM.InHill;
+}
+
+public class InAreaVM : BooleanVM
+{
+	private string filepath = "";
+	public string Filepath
+	{
+		get { return filepath; }
+		set { filepath = value; RaisePropertyChanged(); }
+	}
+
+	public override NodeKindVM Kind => NodeKindVM.InArea;
+}
