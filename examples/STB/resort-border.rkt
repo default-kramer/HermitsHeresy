d33a890a2208b377fe81cfe2ecbaaf19c8fd8c03
Ryan Kramer <default.kramer@gmail.com>
2025-02-27 13:41:57 -0600

    Resort area: border looks good, draw distance solved!

--- examples/STB/resort-border.rkt
+++ examples/STB/resort-border.rkt
@@ -10,16 +10,23 @@
 {begin
   (save-dir "C:/Users/kramer/Documents/My Games/DRAGON QUEST BUILDERS II/Steam/76561198073553084/SD/")
 
+  ; Remember: you can set the PRNG state for a tagged build
+  (pseudo-random-generator->vector (current-pseudo-random-generator))
+
   (copy-all-save-files! #:from 'B02 #:to 'B00)
 
   (define dst (load-stage 'IoA 'B00))
 
+  ; To avoid draw distance problems when looking from the NE corner to the SW
+  ; corner, lower the hill as it goes south.
   (define ph-resort-border-west-lo
     (make-platform-hills (generate-platform-layout 40 150)
                          #:x 560 #:z 290
-                         #:peak-y 44))
+                         ; Was 44, dropped to 39 for draw distance issues.
+                         ; Maybe create another hill at 44 to underlie the `-hi` hill.
+                         #:peak-y 39))
   (define ph-resort-border-west-hi
-    (make-platform-hills (generate-platform-layout 30 150)
+    (make-platform-hills (generate-platform-layout 30 90)
                          #:x 560 #:z 290
                          #:peak-y 54))
 
