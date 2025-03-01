7535384b8c82bc9a7dd2d712eb7662cce8ee5847
Ryan Kramer <default.kramer@gmail.com>
2025-03-01 15:16:41 -0600

    add one-offs.rkt so I don't have to keep recreating these scripts every time

---
+++ examples/STB/one-offs.rkt
@@ -0,0 +1,39 @@
+#lang racket
+
+; Templates for one-off scripts
+
+(require hermits-heresy)
+
+(save-dir "C:/Users/kramer/Documents/My Games/DRAGON QUEST BUILDERS II/Steam/76561198073553084/SD/")
+
+#;{begin ; one-off template: stage->pict
+    (require (submod hermits-heresy undocumented)
+             pict)
+
+    (define stage (load-stage 'IoA 'B02))
+    (define the-pict
+      (let* ([block1 (block 'Straw-Floor)]
+             [block2 (+ #x800 block1)]
+             [color #xFF0000FF])
+        (stage->pict stage (hash block1 color block2 color))))
+    (let ([filename "TEMPAREA.bmp"])
+      (send (pict->bitmap the-pict) save-file filename 'bmp)
+      (println (format "updated: ~a" filename)))
+    }
+
+#;{begin ; one-off template: filling an area
+    (require (submod hermits-heresy undocumented))
+
+    (copy-all-save-files! #:from 'B02 #:to 'B00)
+    (define stage (load-stage 'IoA 'B00))
+
+    (define area (bitmap->area "TEMPAREA.bmp"))
+    (define trav (traversal
+                  (cond
+                    [(and (in-area? area)
+                          (< YYY 35))
+                     (set-block! 'Grassy-Earth)
+                     (set-chisel! 'none)])))
+    (traverse stage trav #:force-unsound-optimization? #t)
+    (save-stage! stage)
+    }
