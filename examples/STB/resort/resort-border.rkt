259ce64602bbe976a70c7791ed208879fb15aacf
Ryan Kramer <default.kramer@gmail.com>
2025-03-16 12:29:34 -0500

    tweak shallow sea contour

--- examples/STB/resort/resort-border.rkt
+++ examples/STB/resort/resort-border.rkt
@@ -131,8 +131,8 @@
       [(recurring) (values 469 ; yellow hardwood tile
                            (block 'Strange-Sand)
                            ; need to clear some more items
-                           0 #;(block 'Sea-water-full-block)
-                           0 #;(block 'Sea-water-shallow-block))]))
+                           (block 'Sea-water-full-block)
+                           (block 'Sea-water-shallow-block))]))
 
   (define do-hills? (member release-mode '(publish)))
 
