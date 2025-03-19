af4bda34760dd1fa63b157bf381de9080bf437fa
Ryan Kramer <default.kramer@gmail.com>
2025-03-18 23:10:02 -0500

    resort: add script to change the "belt" and walls above it, plus notes on choices I considered (am still considering)

--- examples/STB/resort/resort-border.rkt
+++ examples/STB/resort/resort-border.rkt
@@ -118,6 +118,54 @@
                   (not (= 0 (remainder XXX 96)))
                   (not (= 0 (remainder ZZZ 96))))
          (set-block! 120))
+     #;(let ([belt-y 40]
+             ; The only flaw with Sooty Softwood is that I'd prefer not to stack
+             ; brick/stone on top of wood if I can avoid it...
+             [belt (block 'Sooty-Softwood)]
+             ; Granite Floor is almost as good as Sooty Softwood.
+             ; But it does kind of insist upon itself.
+             ; And there is one pretty gnarly reflection from the pool light.
+             #;[belt (block 'Granite-Floor-Block)]
+             ; The Red Brick Floor really pops, but the texture is too
+             ; similar to the Masonry Wall below it.
+             #;[belt (block 'Red-Brick-Floor)]
+
+             ; Mottled Wall (black) pretty much requires a black roof,
+             ; but it's worth it. Interiors and exteriors both pop!
+             ; This + Sooty Softwood + black roofs looks unbeatable.
+             ; Should I try the non-Fancy black roof?
+             ; Nah, I'd rather keep that fresh for my tiny houses.
+             ; (Also: Changed the pool cover and harp cover to the non-Fancy roof (blue)
+             ;  and it matches perfectly, and even ties the blue bunting together!)
+             ; UPON FURTHER REVIEW: Masonry Wall might actually be better?
+             [wall 810] ; Mottled Wall (black)
+             ; Herringbone Floorboard (white) is very good.
+             ; It works with the blue or black roof.
+             ; But I think the texture is just a little too much,
+             ; giving the edge to the Mottled Wall (black).
+             #;[wall 756] ; Herringbone Floorboard (white)
+             ; Don't forget, Masonry Wall is still very good.
+             ; Works with blue or black roof, but I think black is better.
+             #;[wall (block 'Masonry-Wall)]
+             ; Wooden Wall (blue) does make interiors look great.
+             ; Needs roof change, black is good.
+             #;[wall 308] ; Wooden Wall (blue)
+             ; Woodless Timbered Wall is too white,
+             ; although I never tried it with the black roof.
+             #;[wall (block 'Woodless-Timbered-Wall)]
+             ; Always worth trying Adobe, but not this time.
+             #;[wall (block 'Adobe-Wall-Midsection)]
+             ; Other notes: Slimy Wall is actually pretty good, and where
+             ; would I use it other than a resort?
+             ; Even the Slimy Block is better than I would expect.
+             )
+         (cond
+           [(and (= YYY belt-y)
+                 (block-matches? 'Modern-Masonry-Block 'Granite-Floor-Block))
+            (set-block! belt)]
+           [(and (> YYY belt-y)
+                 (block-matches? 'Masonry-Wall))
+            (set-block! wall)]))
      (cond
        [(and do-hills?
              (or (in-platform-hills?! ph-resort-border-north-hi)
